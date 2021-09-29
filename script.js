c689ccb333bd2fb0df18b4a39decd5117334bba0
Colette <83985997+TheCheekyGit@users.noreply.github.com>
2021-09-29 22:57:08 +0100

    Level 350 > 400

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ function setup() {
   //const allEpisodes = getAllEpisodes(); 
   //makePageForEpisodes(allEpisodes); 
   
-  fetch("https://api.tvmaze.com/shows/82/episodes")
+  
+  fetch("https://api.tvmaze.com/shows/87/episodes")
    .then((response) => response.json())
    .then((episodeList) => {
      makePageForEpisodes(episodeList);
+     createDropDownList(episodeList);
    });
 
   let searchEpisodesBox = document.querySelector("#searchEpisodes");
@@ -17,20 +19,57 @@ function setup() {
 
 }
 
+function createOptionForEpisodes (episode){
+  let select = document.getElementById("dropDownMenu");
+  let option = document.createElement("option");
+  option.value = episode.name;
+  option.innerText = episode.name;
+  select.appendChild(option);
+}
+
+function createDropDownList (allEpisodes){
+  allEpisodes.forEach(createOptionForEpisodes);
+  eventListnerForDropDown(allEpisodes)
+  
+}
+
+function eventListnerForDropDown(episodes){
+  let selectId = document.getElementById("dropDownMenu");
+  selectId.addEventListener("change", function(){
+    let optionElement = document.querySelectorAll("option");
+    let names= [];
+    optionElement.forEach((option) => names.push(option.value));
+    let selectedName = names.filter((name) =>selectId.value === name);
+    let getOneEpisode = episodes.filter((episode) => episode.name == selectedName);
+
+      
+      if (getOneEpisode.length === 1){
+        makePageForEpisodes(getOneEpisode);
+      }else{
+        makePageForEpisodes(episodes);
+      }
+  });
+}
+  
 function searchEpisodes(){
    let searchEpisodesBox = document.querySelector("#searchEpisodes");
    console.log(searchEpisodesBox.value);
-
    const allEpisodes = getAllEpisodes();
-   let filteredEpisodes = allEpisodes.filter(filterEpisodes);
+   
+   /*let count = document.querySelector(".searchbtn");
+   let newPara = document.createElement("p");
+    newPara.innerText = `Got ${episodeList.length} episode(s)`;
+    count.appendChild(newPara);*/
+  
+let filteredEpisodes = allEpisodes.filter(filterEpisodes);
     makePageForEpisodes(filteredEpisodes);
 
   }
 
+  
 function filterEpisodes(episode){
- let searchEpisodesBox = document.querySelector("#searchEpisodes");
+    let searchEpisodesBox = document.querySelector("#searchEpisodes");
    console.log(searchEpisodesBox.value); 
-    
    rootElem.innerHTML = "";
 
    if(episode.name.toLowerCase().includes(searchEpisodesBox.value.toLowerCase())){
@@ -41,9 +80,12 @@ function filterEpisodes(episode){
 }
 
 function makePageForEpisodes(episodeList) {
- 
+  
+  rootElem.replaceChildren([]);
   episodeList.forEach(createCard);
+
 }
+
 
 function createCard(episode){
   console.log(episode.image.medium);
@@ -77,9 +119,6 @@ card.className = "episodeList";
 rootElem.appendChild(card);
 }
  }
-
-
-
 
 window.onload = setup;
     
